1c2e27da73e1b6ebbb2767f3d185061e03144882
ADR0207 <catalinadriandumitru@outlook.com>
2025-07-31 15:10:00 +0300

    Categories modified with hover added 2

--- divina_textura/shop/admin.py
+++ divina_textura/shop/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Product, Category, Subcategories, ProductImage
+from .forms import ProductImageInlineFormSet  # folosim formset, nu form
 
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
-    max_num = 10  # maxim 10 imagini
+    max_num = 10
+    formset = ProductImageInlineFormSet  
 
 
 @admin.register(Product)
@@ -13,7 +15,7 @@ class ProductModelAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "slug", "price")
     search_fields = ("name", "description", "slug", "price")
     prepopulated_fields = {"slug": ("name",)}
-    inlines = [ProductImageInline]  
+    inlines = [ProductImageInline]
 
 
 @admin.register(Category)
@@ -23,4 +25,8 @@ class CategoryModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
-admin.site.register(Subcategories)
+@admin.register(Subcategories)
+class SubcategoryModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
--- divina_textura/shop/forms.py
+++ divina_textura/shop/forms.py
@@ -1,14 +1,14 @@
-
 from django import forms
+from .models import ProductImage
+from django.forms.models import BaseInlineFormSet
+
 
 class AddToCartForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=12, initial=1)
 
 
-
 class UpdateQuantityForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=12)
-
 
 
 class IncreaseForm(forms.Form):
@@ -21,3 +21,20 @@ class DecreaseForm(forms.Form):
 
 class EraseCartForm(forms.Form):
     pass
+
+
+
+
+
+class ProductImageInlineFormSet(BaseInlineFormSet):
+    def save_new(self, form, commit=True):
+        images = self.files.getlist(form.add_prefix('image'))
+        product = form.instance.product if hasattr(form.instance, 'product') else form.instance
+
+        instances = []
+        for image in images:
+            instance = ProductImage(product=product, image=image)
+            if commit:
+                instance.save()
+            instances.append(instance)
+        return instances
